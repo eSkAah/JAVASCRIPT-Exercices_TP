537b8db189b516a88b025247dc15b605c2185042
Allan Breuil <allan.breuil@hotmail.fr>
2021-10-25 22:57:38 +0200

    Fonctionnalité tableau de recherche de communes

--- TD6/app.js
+++ TD6/app.js
@@ -67,6 +67,7 @@ function getVilles(){
       let result = document.createElement('option');
       result.textContent = ville.nom;
       result.value = ville.code;
+      result.dataset.codePostal = ville.codePostaux;
       villes.appendChild(result);
     })   
   })
@@ -78,31 +79,81 @@ function getVilles(){
 
 
 function getInfosVille(){
+  const tabVilles = document.getElementById('tabVilles');
+      while(tabVilles.firstChild){
+        tabVilles.removeChild(tabVilles.firstChild);
+      };
+
   let departCode = document.getElementById('departs').value;
   let codeVille = document.getElementById('villes').value;
   let population = document.getElementById('population');
-  let tabVilles = document.getElementById('tabVilles');
-      tabVilles.insertRow();
-      tabVilles.firstChild.insertCell(3);
-
-  console.log(codeVille);
 
   fetch('https://geo.api.gouv.fr/departements/'+ departCode +'/communes',{method: 'get'})
   .then( res => res.json())
   .then( (res) => {
+    let villeSelectionne;
+
+    //On récupère l'objet complet de la ville selectionné dans l'input et on affiche son nom et sa population
+    for(let i = 0; i < res.length; i++){
+      if(res[i].code == codeVille){
+        villeSelectionne = res[i];
+        population.textContent = "La population de '" + villeSelectionne.nom + "' est de " + villeSelectionne.population;
+      }
+    // Pour chaques villes du département
+    }
     res.forEach(ville => {
+
+      // Utilisation d'une fonction de filtrage qui vérifie les codesPostaux entre ceux de la ville selectionnée, et ceux des villes dans la liste
+      if(filtre(villeSelectionne.codesPostaux, ville.codesPostaux) !== -1 && ville.codesPostaux !== undefined){
+
+        //stockage du code postale qui a matché dans une variable car on l'a en retour de fonction
+        const cp = filtre(villeSelectionne.codesPostaux, ville.codesPostaux);
+
+        //Ajout d'une ligne pour la ville et 3 colonnes avec les informations demandées
+        let ligne = tabVilles.insertRow();
+        let colNom = ligne.insertCell();
+        let colCp = ligne.insertCell();
+        let colPop = ligne.insertCell();
+
+        //Ajout du contenu qui se trouve dans notre objet, et dans la variable
+        colNom.textContent = ville.nom;
+        colCp.textContent = cp;
+        colPop.textContent = ville.population;
+
+        tabVilles.appendChild(ligne);
+        
+      }else{
+        console.log('Aucune commune éxistante dans la base de données');
+      }      
       
-
-      if(ville.code === codeVille){
-        population.textContent = "La population de '" + ville.nom + "' est de " + ville.code;
-        }
     })
-  });
-
-
+  }).catch( (err) => {
+    return console.log(err);
+  })
 
 }
 
+
+/**
+ * Permet de vérifier si un des codePostaux entre la ville selectionné et ceux dans la liste Match, si oui on fait un retour de ce code, sinon renvoi -1
+ * @param {Array} selectionne 
+ * @param {Array} objetFiltre 
+ * @returns 
+ */
+function filtre(selectionne, objetFiltre){
+  for(let i = 0; i < selectionne.length; i++){
+    for(let j = 0; j < objetFiltre.length; j++){
+      if(selectionne[i] == objetFiltre[j]){
+        return selectionne[i];
+      }
+    }
+  }
+  return -1;
+}
+
+
+
+// Lancement de la récupération de données via l'API
 getRegions();
 
 
